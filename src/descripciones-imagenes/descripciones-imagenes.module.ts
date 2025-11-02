0be8f4acadd1ed3d2bea3e0286e5a40f220a9548
JuD4Mo <juan_d.munoz_o@uao.edu.co>
2025-11-01 19:26:58 -0500

    feat: add NatsModule for microservices communication and integrate it into DescripcionesImagenesModule

--- src/descripciones-imagenes/descripciones-imagenes.module.ts
+++ src/descripciones-imagenes/descripciones-imagenes.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { DescripcionesImagenesService } from './descripciones-imagenes.service';
 import { DescripcionesImagenesController } from './descripciones-imagenes.controller';
 import { CloudinaryProvider } from './imageProvider/cloudinary.provider';
+import { NatsModule } from 'src/transports/nats.module';
 
 
 @Module({
   controllers: [DescripcionesImagenesController],
   providers: [DescripcionesImagenesService, CloudinaryProvider],
+  imports: [NatsModule],
   exports: [CloudinaryProvider]
 })
 export class DescripcionesImagenesModule {}
